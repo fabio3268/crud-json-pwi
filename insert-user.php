71dac3ab16d276e7b975d91732ee277a8c3f9c2d
Fábio Santos <fabio3268@gmail.com>
2023-01-09 20:36:56 -0300

    Atualiza Perfil - v5.0

--- insert-user.php
+++ insert-user.php
@@ -1,24 +1,3 @@
-<?php
-    $user = filter_input_array(INPUT_POST,FILTER_DEFAULT);
-    if($user){
-        // Verificar se nome e email foram informados e se o email é válido...
-        $user["address"] = ""; // Endereço é informado depois
-        $user["phone"] = ""; // Telefone é informado depois
-        //var_dump($user);
-        $arrayUsers = array();
-        // verifica se o json existe
-        if(file_exists(__DIR__ . "/data-json/cadastroUsuarios.json")) {
-            $stringUsers = file_get_contents(__DIR__ . "/data-json/cadastroUsuarios.json");
-            $arrayUsers = json_decode($stringUsers, true);
-        }
-        $arrayUsers[] = $user;
-        $usersJson = json_encode($arrayUsers);
-        //var_dump($usersJson);
-        $file = fopen(__DIR__ . "/data-json/cadastroUsuarios.json","w+");
-        fwrite($file, $usersJson);
-        fclose($file);
-    }
-?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <?php
